7b20a85349c35e0ae40d7492a7df5ec4e0c3178d
Ivan Rendon <ivanrendon@Ivans-iMac.attlocal.net>
2019-05-18 22:55:42 -0400

    adds questions and modal as well as server file and logic for finding freinds

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+var friends = [
+    {
+        name: "Jennifer",
+        about: "I’m a passionate, kind, and loving teacher who believes in social justice and taking Sundays off.",
+        photo: "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1534&q=80",
+        scores: [1, 2, 1, 2, 1, 4, 1, 1, 1, 1]
+    },
+    {
+        name: "Shayla",
+        about: "If you asked my friends who I am, they would give you three words: hilarious, thoughtful, and adventurous.",
+        photo: "https://images.unsplash.com/photo-1500336624523-d727130c3328?ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80",
+        scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+    },
+    {
+        name: "Diane",
+        about: "From a young age, I’ve had a big heart. In my role as a nurse, I have employed that big heart and created a world of love and compassion around me.",
+        photo: "https://images.unsplash.com/photo-1467632499275-7a693a761056?ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80",
+        scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+    },
+    {
+        name: "Maggie",
+        about: "Funny, kind, grateful, humble… okay maybe not that last one. These are just a few of the words that represent who I strive to be.",
+        photo: "https://images.unsplash.com/photo-1516726817505-f5ed825624d8?ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80",
+        scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+    },
+    {
+        name: "Spencer",
+        about: "I may be young, but I’m wise beyond my years. After growing up surrounded by older siblings, I have learned to be genuine, responsible, and yes, a little feisty.",
+        photo: "https://images.unsplash.com/photo-1555992938-ae7aa7e36d07?ixlib=rb-1.2.1&auto=format&fit=crop&w=1587&q=80",
+        scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    },
+    {
+        name: "Julia",
+        about: "The song “Your Hand in Mine” by Explosions in the Sky is my favorite song of all-time, even if it doesn’t have a single word in it. Tell me about yours.",
+        photo: "https://images.unsplash.com/photo-1504730030853-eff311f57d3c?ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80",
+        scores: [1, 1, 4, 4, 5, 2, 1, 4, 2, 1]
+    },
+    {
+        name: "Samantha",
+        about: "If you need a recommendation for the best taco in town, I’m your girl. Looking for the best margarita recommendation to match my own expertise.",
+        photo: "https://images.unsplash.com/photo-1469460340997-2f854421e72f?ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80",
+        scores: [1, 1, 5, 3, 2, 2, 2, 4, 1, 1]
+    },
+    {
+        name: "Kimberly",
+        about: "I’m the oldest of six siblings and I’m close with each of them. Being a big sister has shaped who I am as a person. (Okay, maybe I’m a little bossy.)",
+        photo: "https://images.unsplash.com/photo-1495298599282-d8920eb5009b?ixlib=rb-1.2.1&auto=format&fit=crop&w=1534&q=80",
+        scores: [1, 1, 5, 3, 2, 2, 2, 4, 1, 1]
+    }
+];  
+
+
+module.exports = friends;
--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 //=========-=-=-=-=-==--==-=--=-=-==-
 var express = require("express");
 var path = require("path");
+var friends = require("./app/data/friends");
+var fs = require("fs");
 
 // =============== Set up Server
 var app = express();
@@ -30,32 +32,69 @@ app.get("/api/friends", function (req, res) {
     return res.json(friends);
 });
 
+ ////// Testing the  Funtional post
+app.post("/api/friends", function (req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newFriend = req.body;
 
+    // for (let i = 0; i < newFriend.scores.length; i++){
+    //     newFriend.scores[i] = parseFloat(newFriend.scores[i])
+    // }    
+    console.log(newFriend);
 
+    const resultsArr = [];
 
-
-
-/// Temp for friens
-var friends = [
-    {
-        routeName: "yoda",
-        name: "Yoda",
-        role: "Jedi Master",
-        age: 900,
-        forcePoints: 2000
-    },
-    {
-        routeName: "darthmaul",
-        name: "Darth Maul",
-        role: "Sith Lord",
-        age: 200,
-        forcePoints: 1200
-    },
-    {
-        routeName: "obiwankenobi",
-        name: "Obi Wan Kenobi",
-        role: "Jedi Master",
-        age: 55,
-        forcePoints: 1350
+    for (let i = 0; i < friends.length; i++) {
+        let difference = 0; 
+        for (let j = 0; j < newFriend.scores.length; j++) {
+            difference += Math.abs(parseFloat(friends[i].scores[j]) - parseFloat(newFriend.scores[j]));
+            
+        }
+        resultsArr[i] = difference;
     }
-];
+    console.log(resultsArr);
+    let friendIndex = resultsArr.indexOf(Math.min.apply(Math, resultsArr));
+
+    console.log(friendIndex);
+    // default friend match is the first friend but result will be whoever has the minimum difference in scores
+    // var bestFriendIndex = 0;
+    // var minimumDifference = 40;
+
+    // // in this for-loop, start off with a zero difference and compare the user and the ith friend scores, one set at a time
+    // //  whatever the difference is, add to the total difference
+    // for (var i = 0; i < friends.length; i++) {
+    //     var totalDifference = 0;
+    //     for (var j = 0; j < friends[i].scores.length; j++) {
+    //         var difference = Math.abs(user.scores[j] - friends[i].scores[j]);
+    //         totalDifference += difference;
+    //     }
+
+    //     // if there is a new minimum, change the best friend index and set the new minimum for next iteration comparisons
+    //     if (totalDifference < minimumDifference) {
+    //         bestFriendIndex = i;
+    //         minimumDifference = totalDifference;
+    //     }
+    // }
+
+    // console.log(friends[bestFriendIndex]);
+
+    friends.push(newFriend);
+
+    res.json(friends[friendIndex]);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
